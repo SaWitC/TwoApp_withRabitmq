e7841b3b76b337fa9f82e4ba96de02bd2163369f
unknown <ozabello21@gmail.com>
2022-11-30 20:39:59 +0300

    Updated consumer used z.Ef current avg insert speed 25000 per second

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 using Consumer.Data;
@@ -8,11 +9,13 @@ using RabbitMQ.Client.Events;
 using Sender.Models;
 
 
+
 public class Program
 {
     private static int total = 0;
     private static int clears = 0;
     private static int i = 0;
+    private static List<ProductModel> list = new();
     //private readonly static AppDbContext _dbContext = new AppDbContext();
     //private readonly static ProductRepository _productRepository = new ProductRepository(_dbContext);
 
@@ -21,12 +24,12 @@ public class Program
     {
         if (i != 0)
         {
-            Console.WriteLine("====================================");
+            //Console.WriteLine("====================================");
             clears++;
             //Console.Clear();
             Console.WriteLine($"{i}/per second");
             Console.WriteLine($"{total / clears} avg");
-            Console.WriteLine("total"+total);
+            Console.WriteLine("total" + total);
             Console.WriteLine("work time " + clears);
 
             i = 0;
@@ -41,20 +44,19 @@ public class Program
         //Console.ReadKey();
 
         var task1 = Task.Run(() => StartTheConsumer(factory));
-        var task2 = Task.Run(() => StartTheConsumer(factory));
-        var task3 = Task.Run(() => StartTheConsumer(factory));
-        var task4 = Task.Run(() => StartTheConsumer(factory));
-        var task5 = Task.Run(() => StartTheConsumer(factory));
-        var task6 = Task.Run(() => StartTheConsumer(factory));
-        var task7 = Task.Run(() => StartTheConsumer(factory));
-        var task8 = Task.Run(() => StartTheConsumer(factory));
+        //var task2 = Task.Run(() => StartTheConsumer(factory));
+        //var task3 = Task.Run(() => StartTheConsumer(factory));
+        //var task4 = Task.Run(() => StartTheConsumer(factory));
+        //var task5 = Task.Run(() => StartTheConsumer(factory));
+
 
 
         // var task3 = Task.Run(() => { var timer = new Timer(TimerCallback, null, 0, 1000); });
 
         //await task3;
 
-        await Task.WhenAll(task1,task2,task3,task4,task5,task6,task7,task8);
+        //await Task.WhenAll(task1,task2,task3,task4,task5);
+        await task1;
 
         //x5 =56%
         //x1=44%
@@ -88,16 +90,43 @@ public class Program
                     i++;
                     var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
-                    var ResponseModel = JsonSerializer.Deserialize<ProductModel>(message);
-                    ResponseModel.Id = Guid.NewGuid().ToString();
+                    var responseModel = JsonSerializer.Deserialize<ProductModel>(message);
+                    responseModel.Id = Guid.NewGuid().ToString();
 
                     using (AppDbContext dbContext = new AppDbContext())
                     {
 
-                        dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
-                        ProductRepository productRepository = new ProductRepository(dbContext);
-                        await productRepository.Create(ResponseModel);
+                        //    //Console.WriteLine(list.Count());
+                        if (list.Count() >= 1000)
+                        {
+                            await dbContext.BulkInsertAsync<ProductModel>(list, opt =>
+                            {
+                                opt.BatchSize = 1000;
+
+                            });
+                            list.Clear();
+                            //await dbContext.SaveChangesAsync();
+                        }
+                        else
+                        {
+                            list.Add(responseModel);
+                        }
+
+
+                        //    //dbContext.BulkInsert(customers, options => {
+                        //    //    options.InsertIfNotExists = true;
+                        //    //    options.PrimaryKeyExpression = customer => customer.Code;
+                        //    //});
+                        //    //dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
+                        //    //ProductRepository productRepository = new ProductRepository(dbContext);
+                        //    //await productRepository.Create(ResponseModel);
                     };
+
+
+
+
+
+
                     channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 };
 
